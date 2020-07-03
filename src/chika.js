38baf0267138c89ac71b7a255ed21b61fe8bdfa8
Kazumi Inada <hello@nandenjin.com>
2020-07-03 11:38:25 +0900

    [Chore] Lintfix

--- src/chika.js
+++ src/chika.js
@@ -1,71 +1,51 @@
-
 const chalk = require('chalk')
-const consola = require('consola');
+const consola = require('consola')
 const ArtNet = require('artnet')
 
-function chika({
-  host,
-  universe = 1,
-  interval,
-  min,
-  max,
-  version,
-  sendAll,
-  refreshInterval
-}, {
-  input,
-  showHelp,
-  showVersion
-}) {
-
-  if(version) showVersion()
+function chika(
+  { host, universe = 1, interval, min, max, version, sendAll, refreshInterval },
+  { input, showHelp, showVersion }
+) {
+  if (version) showVersion()
 
   universe /= 1
   interval /= 1
 
   refreshInterval /= 1
 
-  if(sendAll) {
+  if (sendAll) {
     consola.info('sendAll flag specified. Transmitting all channels...')
   }
 
   const artnet = ArtNet({
     host,
     universe,
-    refresh: refreshInterval
+    refresh: refreshInterval,
   })
 
-  const addrs = [];
+  const addrs = []
 
-  if(input.length === 0) {
+  if (input.length === 0) {
     consola.error('No address specified')
     showHelp()
   }
 
   input.forEach(ins => {
-    ins.split(',')
-      .forEach(a => {
+    ins.split(',').forEach(a => {
+      if (a.match(/^([0-9]+)-([0-9]+)$/)) {
+        const startAddr = Math.min(+RegExp.$1, +RegExp.$2)
+        const endAddr = Math.max(+RegExp.$1, +RegExp.$2)
 
-        if(a.match(/^([0-9]+)\-([0-9]+)$/)) {
-
-          const startAddr = Math.min(+RegExp.$1, +RegExp.$2)
-          const endAddr = Math.max(+RegExp.$1, +RegExp.$2)
-
-          for(let i = startAddr; i <= endAddr; i++) {
-            addrs.push(i)
-          }
-
-        }else if(a.match(/^[0-9]+$/)) {
-
-          addrs.push(a/1)
-
-        }else {
-
-          consola.error('Invalid address')
-          showHelp()
-
+        for (let i = startAddr; i <= endAddr; i++) {
+          addrs.push(i)
         }
-      })
+      } else if (a.match(/^[0-9]+$/)) {
+        addrs.push(a / 1)
+      } else {
+        consola.error('Invalid address')
+        showHelp()
+      }
+    })
   })
 
   consola.log(`Universe: ${universe}`)
@@ -75,25 +55,24 @@ function chika({
   let csr = 0
 
   setInterval(() => {
-
     vs[csr] = !vs[csr]
     csr++
-    if(csr >= vs.length) csr = 0;
+    if (csr >= vs.length) csr = 0
 
-    let output = '\r' + addrs.map((a, i)=>{
-
-      const addrStr = ('   ' + a).slice(-3)
-      if(vs[i]) return chalk.bgWhite(chalk.black(addrStr))
-      else return chalk.black(chalk.white(addrStr))
-
-    }).join(' ')
+    let output =
+      '\r' +
+      addrs
+        .map((a, i) => {
+          const addrStr = ('   ' + a).slice(-3)
+          if (vs[i]) return chalk.bgWhite(chalk.black(addrStr))
+          else return chalk.black(chalk.white(addrStr))
+        })
+        .join(' ')
 
     process.stdout.write(output)
 
     vs.forEach((v, i) => artnet.set(addrs[i], v ? max : min))
-
   }, interval * 1000)
-
 }
 
-module.exports = chika;
+module.exports = chika
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 #! /usr/bin/env node
 
 const meow = require('meow')
-const chika =  require('./chika')
+const chika = require('./chika')
 
-const cli = meow(`
+const cli = meow(
+  `
 Usage:
   $ achika -h [HOST IP] [ADDRESS1] [ADDRESS2] [...]
 
@@ -12,53 +13,53 @@ Usage:
   --universe, -u [UNIVERSE] Set universe
   --interval, -i  [INTERVAL SEC] Set interval in seconds
   --max [MAX VALUE] / --min [MIN VALUE] Set max/min value
-`, {
-  flags: {
+`,
+  {
+    flags: {
+      version: {
+        type: 'boolean',
+        alias: 'v',
+      },
 
-    version: {
-      type: 'boolean',
-      alias: 'v'
+      host: {
+        type: 'string',
+        alias: 'h',
+        default: '255.255.255.255',
+      },
+
+      universe: {
+        type: 'string',
+        alias: 'u',
+        default: 1,
+      },
+
+      interval: {
+        type: 'string',
+        alias: 'i',
+        default: 1,
+      },
+
+      max: {
+        type: 'string',
+        default: 255,
+      },
+
+      min: {
+        type: 'string',
+        default: 0,
+      },
+
+      sendAll: {
+        type: 'boolean',
+        default: false,
+      },
+
+      refreshInterval: {
+        type: 'string',
+        default: 1000 / 60,
+      },
     },
-
-    host: {
-      type: 'string',
-      alias: 'h',
-      default: '255.255.255.255'
-    },
-
-    universe: {
-      type: 'string',
-      alias: 'u',
-      default: 1
-    },
-
-    interval: {
-      type: 'string',
-      alias: 'i',
-      default: 1
-    },
-
-    max: {
-      type: 'string',
-      default: 255
-    },
-
-    min: {
-      type: 'string',
-      default: 0
-    },
-
-    sendAll: {
-      type: 'boolean',
-      default: false
-    },
-
-    refreshInterval: {
-      type: 'string',
-      default: 1000/60
-    }
-
   }
-} );
+)
 
-chika(cli.flags, cli);
+chika(cli.flags, cli)
